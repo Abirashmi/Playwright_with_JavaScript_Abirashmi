eacb71a88d7f38c4412fa573a551a94cbdb4f322
Abirashmi <abirashmi98@gmail.com>
2026-05-15 08:22:19 +0530

    changes

--- tests/ExcelReader.spec.js
+++ tests/ExcelReader.spec.js
@@ -1,10 +1,10 @@
-import {test,except} from '@playwright/test'
-import *as XLSX from 'xlsx'
+// import {test,except} from '@playwright/test'
+// import *as XLSX from 'xlsx'
 
-const workbook = XLSX.readFile('testdata.xlsx')
-const sheetname = workbook.SheetNames[0]
-const sheet = workbook.Sheets[sheetname]
-const data = XLSX.utils.sheet_to_json(sheet)
+// const workbook = XLSX.readFile('testdata.xlsx')
+// const sheetname = workbook.SheetNames[0]
+// const sheet = workbook.Sheets[sheetname]
+// const data = XLSX.utils.sheet_to_json(sheet)
 
 
 
